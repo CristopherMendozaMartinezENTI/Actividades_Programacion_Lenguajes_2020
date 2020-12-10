8da27d2157beffc2c6d491d7574130f82d0ccfe8
TheKrizXP1 <thekrisxp1@gmail.com>
2020-12-10 13:42:31 +0100

    Identation minor fix

--- ActividadesProg2020/src/AA1_03/GameManager.h
+++ ActividadesProg2020/src/AA1_03/GameManager.h
@@ -10,40 +10,15 @@ class GameManager
 {
 private:
 	Scene* currentScene;
-	Scene* LastScene;
-
-	//Music
-	AudioManager music;
-
 	InputManager inputs;
-
-	//Colors
-	std::map<std::string, Color> colors;
-
-	//Texts
-	std::map<std::string, Text> texts;
-			
-	//Rects
-	std::map<std::string, Rect> rectangles;
-	Rect coinRect[AMOUNT_OF_COINS];
-
 	//ScoreBoards
 	ScoreBoard boardP1;
 	ScoreBoard boardP2;
-
 	//Players
 	PlayerClass playerClass1;
 	PlayerClass playerClass2;
-
 	gameStates state;
-	Vec2 mouseAxis;
-	bool isRunning, mouseClicked, playMenuMusic;
-	bool playHover, sondOffHover, sondOnHover, exitHover;
-
-	int timeDown, playTime;
-
-	clock_t lastTime;
-	float deltaTime;
+	bool isRunning;
 
 public:
 	GameManager();
--- ActividadesProg2020/src/AA1_03/SceneGame.h
+++ ActividadesProg2020/src/AA1_03/SceneGame.h
@@ -8,18 +8,13 @@ private:
 	//ScoreBoards
 	ScoreBoard boardP1;
 	ScoreBoard boardP2;
-
 	//Players
 	PlayerClass playerClass1;
 	PlayerClass playerClass2;
-
 	Rect coinRect[AMOUNT_OF_COINS];
-
 	Vec2 mouseAxis;
-
-	int timeDown;
-
 	clock_t lastTime;
+	int timeDown;
 	float deltaTime;
 
 public:
